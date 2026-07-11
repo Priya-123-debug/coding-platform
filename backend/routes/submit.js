c903485121b5a280b1b6303a43870c16ee098678
priya-123-debug <supriyakum880@gmail.com>
2026-07-12 01:04:50 +0530

    fiix mistake of name

--- backend/routes/submit.js
+++ backend/routes/submit.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const usermiddleware = require("../middleware/usermiddleware");
-const { submitcode, runcode } = require("../Controllers/usersubmission");
+const { submitcode, runcode,getSubmissionsByProblem } = require("../Controllers/usersubmission");
 const submitrouter = express.Router();
 submitrouter.post("/submit/:id", usermiddleware, submitcode);
-submitrouter.get("/problem/:id", usermiddleware, submissionController.getSubmissionsByProblem);
+submitrouter.get("/problem/:id", usermiddleware, getSubmissionsByProblem);
 submitrouter.post("/run/:id", usermiddleware, runcode);
 
 module.exports = submitrouter;
